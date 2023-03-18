4a8b348b86e5b736fac3db25f33b9c85625bf580
Akshay Neje <akshayneje2823@gmail.com>
2023-03-18 18:04:15 +0530

    Just started

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import './App.css'
-// import Homepage from "./components/homepage/homepage"
-import Login from "./Components/Auth/Login/login"
-import Register from "./Components/Auth/Register/Registration"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
+import Home from './pages/Home';
 
 function App() {
 
-  const [ user, setLoginUser] = useState({})
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          {/* <Route exact path="/"> */}
-            {/* {
-              user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
-            } */}
-          {/* </Route> */}
-          <Route path="/login" element={<Login setLoginUser={setLoginUser}/>}/>
-          
-
-          <Route path="/register" element={<Register/>}/>
-            
-        </Routes>
-      </Router>
+      <Home/>
     </div>
   );
 }
